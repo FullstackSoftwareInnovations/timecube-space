e4db2391233f9ddee15975ab8aad9c73f08b4f2e
Fullstack Chris <chris@fullstack.software>
2022-04-06 21:13:14 -0600

    timecube site init

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React from "react";
+import './App.css'
+
+const timecube = require('timecube')
+
+function App() {
+    let chunks = timecube.generateChunks(timecube.maxChunks, 0, timecube.maxChunks, true)
+
+    let first = chunks.shift()
+    chunks.unshift(chunks.splice(timecube.maxChunks -2, 1)[0])
+
+    return (
+        <div className="App" style = {{maxWidth: 500, margin: '0 auto', padding: "0 40px", textAlign: 'left'}}>
+            <p style = {{fontSize: '2.25em', fontWeight: 1000}}>
+                {first}
+            </p>
+
+            { chunks.map((chunk)=> <TimeCubeChunk text = {chunk}/> )}
+        </div>
+    );
+}
+
+function TimeCubeChunk(props){
+    return (
+        <p style = {{fontSize: '1.8em', fontWeight: 700, maxWidth: 500, whiteSpace: 'pre-line'}}>
+            {props.text}
+            <br/>
+            <br/>
+            ***********************
+        </p>
+    )
+}
+
+export default App;
